02fde31564de43a438b1cd766dc870f4c90321fd
José Roberto <jose.roberto@e-smart.com.br>
2019-09-12 15:03:23 -0300

    Add custom status param to all order handler actions

--- src/Api/DataTransformer/Sales/Order/ShipmentException.php
+++ src/Api/DataTransformer/Sales/Order/ShipmentException.php
@@ -28,8 +28,9 @@ class ShipmentException extends DataTransformerAbstract
      * @param string $orderId
      * @param string $datetime
      * @param string $observation
+     * @param string $status
      */
-    public function __construct($orderId, $datetime, $observation)
+    public function __construct($orderId, $datetime, $observation, $status)
     {
         /**
          * @todo Convert the $datetime to the correct format: '2012-10-06T04:13:00-03:00'
@@ -38,7 +39,8 @@ class ShipmentException extends DataTransformerAbstract
             'shipment_exception' => [
                 'occurrence_date' => $datetime,
                 'observation'     => $observation
-            ]
+            ],
+            'status' => $status
         ];
 
         $this->setOutputData($shipmentException);
