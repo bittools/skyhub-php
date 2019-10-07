079f95166ba00357d23bf3b82c6348ad06e67506
Tiago Sampaio <tiago@tiagosampaio.com>
2019-10-07 13:11:23 -0300

    Changes:
    - Created the Http Client Builder layer.

---
+++ src/Api/Service/ClientBuilderInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SkyHub\Api\Service;
+
+/**
+ * Class ClientBuilderInterface
+ *
+ * @package SkyHub\Api\Service
+ */
+interface ClientBuilderInterface
+{
+    /**
+     * @param string $baseUri
+     * @param array  $defaults
+     *
+     * @return \GuzzleHttp\Client
+     */
+    public function build($baseUri = null, array $defaults = []);
+}
